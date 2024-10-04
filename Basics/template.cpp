10df7acbc909d0460cd7d63ee534b74f2f04d365
Mahmudul Hasan Bhuiyan <107288719+HASAN-49@users.noreply.github.com>
2024-10-04 21:18:26 +0600

    My template for competitive programming

---
+++ Basics/template.cpp
@@ -0,0 +1,45 @@
+// بسم الله الرحمن الرحيم
+#include<bits/stdc++.h>
+using namespace std;
+
+#define test int t; for(cin >> t; t; t--)
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+
+#define all(a) a.begin(), a.end() 
+#define allr(a) a.rbegin(), a.rend()
+#define out(x) cout << x << endl
+#define outs(x) cout << x << " "
+
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+#define endl '\n'
+#define nline cout << '\n'
+
+#define pi 3.14159265358979323846
+#define Mm 998244353
+const int M = 1e9 + 7;
+
+int dx[] = {-1, 1, 0, 0, -1, -1, 1, 1};
+int dy[] = {0, 0, 1, -1, -1, 1, -1, 1};
+
+void solve(int tc) {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    // #endif
+
+    int k = 1;
+    // test 
+    solve(k++);
+
+    return 0;
+}
