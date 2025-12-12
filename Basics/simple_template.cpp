2d476597430a13c5cdeaae014480c20c61ccfaee
Mahmudul Hasan Bhuiyan <107288719+HASAN-49@users.noreply.github.com>
2025-12-12 10:35:16 +0600

    simple template

---
+++ Basics/simple_template.cpp
@@ -0,0 +1,18 @@
+// بسم الله الرحمن الرحيم
+#include<bits/stdc++.h>
+using namespace std;
+
+#define test int t; for(cin >> t; t; t--)
+
+void solve() {
+  
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  solve();
+
+  return 0;
+}
